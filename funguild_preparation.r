dc1f386fd1e45997b307309920a1c04b25d91f0a
Chikae <65932847+Chikae-Tatsumi@users.noreply.github.com>
2020-07-17 14:21:57 +0900

    Add files via upload

---
+++ funguild_preparation.r
@@ -0,0 +1,8 @@
+setwd("~/R/Analysis/1_Test/ITS")
+ASV.table <- read.table(file="rarefied_ASV_table.txt")
+taxonomy <- paste(ASV.table$Kingdom, ASV.table$Phylum, ASV.table$Class, ASV.table$Order, ASV.table$Family, ASV.table$Genus, ASV.table$Species, sep="; ")
+ASV.table <- ASV.table [,1:(ncol(ASV.table)-7)] 
+ASV.table <- cbind (ASV.table, taxonomy)
+ASV.table <- cbind (rownames(ASV.table),ASV.table)
+colnames(ASV.table)[1] <- "OTU ID"
+write.table (ASV.table, file="rarefied_ASV_table_before_funguild.txt",sep="\t",row.names=F, quote=F)
